180a4782ffaebe5738352b8cf4cd2a0701915d37
Sobakin <c0mrade.Sobakin@yandex.ru>
2022-04-24 18:06:27 +0300

    cool kata! :)

--- Create Phone Number.py
+++ Create Phone Number.py
@@ -6,6 +6,8 @@
 # The returned format must be correct in order to complete this challenge.
 # Don't forget the space after the closing parentheses!
 
+# мое решение
+
 def create_phone_number(n):
     result = ["("]
     if len(n) == 10:
@@ -18,4 +20,30 @@ def create_phone_number(n):
         return "".join(result)
 
 
-print(create_phone_number([1,2,3,4,5,6,7,8,9,0]))
+print(create_phone_number([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+
+
+# Решения других пацанов
+
+"""
+def create_phone_number(n):
+    return "({}{}{}) {}{}{}-{}{}{}{}".format(*n)
+------------------------------------------------------------------------------------------------------------------------
+
+def create_phone_number(n):
+    n = ''.join(map(str,n))
+    return '(%s) %s-%s'%(n[:3], n[3:6], n[6:])
+------------------------------------------------------------------------------------------------------------------------
+
+    def create_phone_number(n):
+  str1 =  ''.join(str(x) for x in n[0:3])
+  str2 =  ''.join(str(x) for x in n[3:6])
+  str3 =  ''.join(str(x) for x in n[6:10])
+
+
+  return '({}) {}-{}'.format(str1, str2, str3)
+------------------------------------------------------------------------------------------------------------------------
+
+def create_phone_number(n):
+    return "(%i%i%i) %i%i%i-%i%i%i%i" % tuple(n)
+"""
